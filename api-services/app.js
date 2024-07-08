5c6d212812fc2f3c134f2ce55af6145fb45af55d
dustae <dout.vgi@gmail.com>
2024-07-08 15:02:59 +0700

    init firebase sdk

--- api-services/app.js
+++ api-services/app.js
@@ -2,14 +2,42 @@ const express = require('express')
 const app = express()
 const { readdirSync } = require('fs')
 
+const serviceAccount = require('./key.json');
+const admin = require('firebase-admin');
+
+admin.initializeApp( { 
+    credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore();
+
+const extractKeys = (obj, keys) => {
+    return keys.reduce((acc, key) => {
+      if (obj.hasOwnProperty(key)) {
+        acc[key] = obj[key];
+      }
+      return acc;
+    }, {});
+  };
+
+
 
 // read routes from /Routes
-readdirSync('./Routes').map((r) => app.use('/api', require('./Routes/' + r)))
+// readdirSync('./Routes').map((r) => app.use('/api', require('./Routes/' + r)))
 
 
+(async () => {
+    try {
+        const response = db.collection('event');
+        const doc = await response.get();
+        console.log(doc.query);
+    } catch (error) {
+        console.error(error);
+    }
+})();
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen( PORT, () => { 
     console.log(`Server is running on PORT ${PORT}.`);
 })
