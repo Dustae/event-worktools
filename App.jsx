5d58d64953de4234326e704d21e736d6ff7e81c9
SuperYoo <93319356+MaserAUA@users.noreply.github.com>
2024-07-18 16:41:46 +0700

    Delete old ver
    eiei

--- App.jsx
+++ App.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import RegisterBooth from './booth/RegisterBooth';
-import LoginBooth from './booth/LoginBooth';
-import ConfirmBooth from './booth/ConfirmBooth';
-import SuccessBooth from './booth/SuccessBooth';
-import CheckIn from './Worktool/CheckIn';
-import Register from './Worktool/Register';
-import Page404 from './Worktool/Page404';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
+import RegisterBooth from './booth/RegisterBooth/RegisterBooth';
+import LoginBooth from './booth/LoginBooth/LoginBooth';
+import ConfirmBooth from './booth/ConfirmBooth/ConfirmBooth';
+import SuccessBooth from './booth/SuccesBooth/SuccessBooth';
+import CheckIn from './Worktool/Checkin/CheckIn';
+import Register from './Worktool/Register/Register';
+import Page404 from './Worktool/Page404/Page404';
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 import About from './components/About';
 import Promo from './components/Promo';
 import Team from './components/Team';
 import Statistics from './components/Statistics';
-import Work from './components/Work';
+import Work from './components/Work/Work';
 import Skill from './components/Skill';
 import Pricing from './components/Pricing';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import CustomerLayout from './components/CustomerLayout';
-
-
+import WorktoolLayout from './components/WorktoolLayout';
+import Home from './Worktool/HomeDashboard/Home';
+import Create from './Worktool/Create/Create';
+import EventAnalytic from './Worktool/EventDashboard/EventAnalytic';
+import EventDetail from './Worktool/EventDashboard/EventDetail';
+import DataTable from './Worktool/EventDashboard/DataTable';
 import './App.css';
+import FormDesign from './Worktool/FormDesign/FormDesign';
+import PageCreate from './Worktool/Create/PageCreate';
+import DashBoard from './Worktool/EventDashboard/DashBoard';
+import Summary from './Worktool/HomeDashboard/Summary';
+import PageForm from './Worktool/FormDesign/PageForm';
+import Setting from './Worktool/SettingProfile/Setting';
+import PageSetting from './Worktool/SettingProfile/PageSetting';
+import SuccessRegister from './booth/SuccesBooth/SuccessRegister';
+
 
 const App = () => {
   const location = useLocation();
-  const showNavbar = !['/registerbooth', '/loginbooth', '/confirmbooth', '/successbooth', '/checkin', '/register', '*'].includes(location.pathname);
+  const showNavbar = !['/registerbooth', '/loginbooth', '/confirmbooth', '/successbooth', '/checkin', '/register', '/home', '/home/create', '/eventanalytic' , '/eventdetail' , '/datatable' , '/formdesign' , '/pagecreate', '/dashboard', '/summary', '/PageForm', '/setting', '/pagesetting','*'].includes(location.pathname);
 
   return (
     <>
@@ -43,6 +56,21 @@ const App = () => {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/footer" element={<Footer />} />
+        
+        <Route element={<WorktoolLayout/>}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/create" element={<Create />} />
+        <Route path="/eventanalytic" element={<EventAnalytic />} />
+        <Route path="/eventdetail" element={<EventDetail />} />
+        <Route path="/datatable" element={<DataTable />} />
+        <Route path="/formdesign" element={<FormDesign />} />
+        <Route path="/pagecreate" element={<PageCreate />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/summary" element={<Summary />} />
+        <Route path="/pageform" element={<PageForm />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/pagesetting" element={<PageSetting />} />
+        </Route>
  
         {/* Customer routes */}
         <Route element={<CustomerLayout />}>
@@ -50,6 +78,7 @@ const App = () => {
           <Route path="/loginbooth" element={<LoginBooth />} />
           <Route path="/confirmbooth" element={<ConfirmBooth />} />
           <Route path="/successbooth" element={<SuccessBooth />} />
+          <Route path="/registersuccess" element={<SuccessRegister/>} />
         </Route>
         {/* Page not found route */}
         <Route path="*" element={<Page404 />} />
@@ -59,4 +88,3 @@ const App = () => {
 };
 
 export default App;
-
--- main.jsx
+++ main.jsx
@@ -4,7 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import 'w3-css/w3.css';
 import 'aos/dist/aos.css';
-import 'react-image-lightbox/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css';
+
+
+
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
